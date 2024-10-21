e8b05ada3db91f5106fa252b398fccea1e07d36e
Clàudia Gómez <claudia.gomez04@estudiant.upf.edu>
2024-10-21 14:05:31 +0200

    final attempt

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,45 +1,48 @@
+## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
+## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
+## COMMAND LINE SPECIFIED BELOW
 import sys
 
 def main() :
-
-    #Si hi ha arguments passats per la línia de comandes
-    x = int(sys.argv[1])  #Agafa el primer argument passat com a número
-    
-    # Inicialitza variables
+    ## YOU CODE SHOULD START HERE AST THE SAME
+    ## IDENTATION AS THIS COMMENT
+    x = int(sys.argv[1])  # Agafem el valor de la línia de comandes
 
     def comptar_divisors(x):
-        i = 1
-        divisors_x = 0
-    # Compta el nombre de divisors de x
-        while i <= x:
-            if x % i == 0:
-                divisors_x += 1
-            i += 1
-        return divisors_x #ensenya el nombre de divisors que té
+        c = 0
+        divisor = 1
+        while divisor <= x:
+            if x % divisor == 0:
+                c += 1
+            divisor += 1
+        return c
 
+    # Comptem els divisors de n
+    divisors_x = comptar_divisors(x)
+    es_antiprime = True  # Suposem que n és antiprime
+    i = 1
+    while i < x:
+        if comptar_divisors(i) >= divisors_x:
+            es_antiprime = False  # Si algun nombre més petit té tants o més divisors, no és antiprime
+            break  # Ja podem parar la cerca
+        i += 1
 
-#Ara he comptat els divisors només de x, vull saber els divisors dels seus nombres inferiors:
-#Perquè ens guardi el número de divisors_x, com que s'elimina, creeem una nova variable que és divisors_x2 perquè ens el guardi i el compararem més tard.
-
-    divisors_x2 = comptar_divisors(x)
-
- #fem una suposició:
-    
-    es_antiprime = True
-    comptador = 1
-    while comptador < x:
-        if comptar_divisors(x) >= divisors_x2:
-            es_antiprime = False
-            break
-        comptador += 1   
-
+    ## THE LAST LINES OF YOUR CODE SHOULD EITHER
+    ## RETURN THE VALUE "anti-prime" or "not anti-prime"
+    ## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
+    ## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
+    ## "anti-prime" or "not anti-prime"
     if es_antiprime:
-        return ("anti-prime")
+        return "anti-prime"
     else:
-        return ("not anti-prime")
-
+        return "not anti-prime"
 
 ## DO NOT REMOVE THIS LINE BELOW
-if __name__ == "__main__":
-    # Crida la funció principal i imprimeix el resultat
+if __name__ == "__main__" :
+
+    ## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
+    ## TO RUN THIS PROGRAM AS, FOR INSTANCE:
+    ## $ python antiprime.py 6
+    ## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
+    ## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
     print(main())
